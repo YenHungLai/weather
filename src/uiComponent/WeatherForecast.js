c7f3bbe1a92af32aff4b59018f1c7e608d4c9e65
YenHungLai <jacoblai1029@gmail.com>
2019-04-06 23:06:56 -0700

    added fadein effect to carousel

--- src/uiComponent/WeatherForecast.js
+++ src/uiComponent/WeatherForecast.js
@@ -45,7 +45,7 @@ const WeatherForecast = (props) => {
     };
 
     return (
-        <div className="card carousel-item">
+        <div className="card carousel-item animated fadeIn">
             <div className="card-content flex flex-col items-start">
                 <h1 className='self-center'>{date.getHours()}:00 {weekday} {date.getMonth()+1}/{date.getDate()}</h1>
                 <span className="card-title self-center"><strong>{name}</strong></span>
